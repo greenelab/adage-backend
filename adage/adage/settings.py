fdf61544ef2ce30991503d74c3134dd1938798ec
dongbohu <dongbo.hu@gmail.com>
2019-10-01 14:09:10 -0400

    Add cors middleware

--- adage/adage/settings.py
+++ adage/adage/settings.py
@@ -58,6 +58,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
