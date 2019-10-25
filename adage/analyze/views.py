0d2db18a2a0225775de6bfab3c807d49ad346af0
dongbohu <dongbo.hu@gmail.com>
2019-10-25 15:00:24 -0400

    Gene search API

--- adage/analyze/views.py
+++ adage/analyze/views.py
@@ -9,13 +9,18 @@ from .serializers import ExperimentSerializer, MLModelSerializer
 
 
 class ExperimentViewSet(ReadOnlyModelViewSet):
+    """Experiments viewset"""
     http_method_names = ['get']
     serializer_class = ExperimentSerializer
     filter_backends = (filters.SearchFilter, )
 
     def get_queryset(self):
         queryset = Experiment.objects.all()
-        # 'search' parameter
+        # 'search' parameter, which does full text search based on the
+        # following 3 fields in Experiment model:
+        # - "accession"
+        # - "name"
+        # - "description"
         search_str = self.request.query_params.get('search', None)
         if search_str is not None:
             from django.contrib.postgres.search import (
@@ -23,20 +28,21 @@ class ExperimentViewSet(ReadOnlyModelViewSet):
             )
             # In AWS Postgres RDS, the default config is 'simple'.
             # We use 'english' to make the text search more flexible.
-            vector = SearchVector('accession', 'name', 'description',
-                                  config='english'
+            vector = SearchVector(
+                'accession', 'name', 'description',
+                config='english'
             )
             query = SearchQuery(search_str, config='english')
             queryset = queryset.annotate(
                 rank=SearchRank(vector, query)
-            ).filter(rank__gte=0.01
+            ).filter(rank__gte=0.05
             ).order_by('-rank', 'accession')
 
         return queryset
 
 
 class MLModelViewSet(ReadOnlyModelViewSet):
-    """Machine learning model viewset"""
+    """Machine learning models viewset"""
 
     http_method_names = ['get']
     queryset = MLModel.objects.all()
--- adage/genes/views.py
+++ adage/genes/views.py
@@ -1,14 +1,57 @@
-# Create your views here.
-from rest_framework import filters
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework.viewsets import ReadOnlyModelViewSet
-
 from .models import Gene
 from .serializers import GeneSerializer
 
 
 class GeneViewSet(ReadOnlyModelViewSet):
+    """Genes viewset"""
+
     http_method_names = ['get']
-    queryset = Gene.objects.all()
     serializer_class = GeneSerializer
+
+    def get_queryset(self):
+        queryset = Gene.objects.all()
+
+        # 'search' parameter, which does full text search based on the
+        # following four fields in Gene model:
+        # - "standard_name": highest priority (A: 1.0);
+        # - "systematic_name": second highest priority (B: 0.4);
+        # - "description" and "crossref__xrid": lowest priority (C: 0.2).
+        search_str = self.request.query_params.get('search', None)
+        if search_str is not None:
+            from django.contrib.postgres.search import (
+                SearchQuery, SearchRank, SearchVector
+            )
+
+            standard_vector = SearchVector('standard_name', weight='A')
+            systematic_vector = SearchVector('systematic_name', weight='B')
+            other_vector = SearchVector(
+                'description', 'crossref__xrid', weight='C'
+            )
+
+            vectors = standard_vector + systematic_vector + other_vector
+            query = SearchQuery(search_str)
+            queryset = queryset.annotate(
+                rank=SearchRank(vectors, query)
+            ).filter(rank__gte=0.1
+            ).order_by('-rank', 'standard_name')
+
+        # 'autocomplete' parameter, which does trigram search based on the
+        # following three fields in Gene model:
+        # - "standard_name" (multiplied by 2.0 to give it a higher priority)
+        # - "systematic_name"
+        # - "description"
+        similarity_str = self.request.query_params.get('autocomplete', None)
+        if similarity_str is not None:
+            from django.contrib.postgres.search import TrigramSimilarity
+
+            queryset = queryset.annotate(
+                similarity=(
+                    TrigramSimilarity('standard_name', similarity_str) * 2.0 +
+                    TrigramSimilarity('systematic_name', similarity_str) +
+                    TrigramSimilarity('description', similarity_str)
+                )
+            ).filter(similarity__gte=0.1
+            ).order_by('-similarity', 'standard_name')
+
+        return queryset
