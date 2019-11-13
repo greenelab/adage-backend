ee1738fe0087aef2e09e80a9e6bf7eda7b2fe43f
dongbohu <dongbo.hu@gmail.com>
2019-11-13 12:00:49 -0500

    tweak dostring again

--- adage/analyze/management/commands/add_samples_info_to_experiment.py
+++ adage/analyze/management/commands/add_samples_info_to_experiment.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 """This management command collects information of samples that are
-associated with each experiment and combines them into a string, then set
-it as the value of `samples_info` field in `Experiment` model. This field
-will be used for full text search in `Experiment` API. The samples information
-includes:
+associated with each experiment and combines them into a string, which is
+then set as the value of `samples_info` field in `Experiment` model. This
+field will be used for full text search in `Experiment` API. The samples
+information includes:
   * sample name and ml_data_source
   * annotation values associated with each sample
 
