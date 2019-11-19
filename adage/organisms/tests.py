fe65e4d51003f6ef139916b7b913bf48ed664861
dongbohu <dongbo.hu@gmail.com>
2019-11-19 14:30:09 -0500

    Migrate management command, add a simple test case

--- adage/organisms/tests.py
+++ adage/organisms/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from organisms.models import Organism
 
-# Create your tests here.
+class ModelTest(TestCase):
+    def test_create(self):
+        foo = Organism.objects.create(
+            taxonomy_id=123,
+            common_name="foo common",
+            scientific_name="foo scientific",
+            slug="foo-slug"
+        )
+
+        bar = Organism.objects.create(
+            taxonomy_id=456,
+            common_name="bar common",
+            scientific_name="bar scientific",
+            slug="bar-slug"
+        )
+
+        self.assertEqual(Organism.objects.count(), 2)
+        self.assertEqual(Organism.objects.first().taxonomy_id, 123)
+        self.assertEqual(Organism.objects.last().slug, "bar-slug")
