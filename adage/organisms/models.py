aaccecb94b04d3c15d6ea7f090fd0810e83ca212
dongbohu <dongbo.hu@gmail.com>
2020-03-24 13:03:57 -0400

    Add url_template in Organism model (#44)

--- adage/organisms/models.py
+++ adage/organisms/models.py
@@ -22,11 +22,11 @@ class Organism(models.Model):
         help_text="Taxonomy ID assigned by NCBI")
 
     common_name = models.CharField(
-        max_length=60, null=False, unique=True,
+        max_length=60, unique=True,
         help_text="Organism common name, e.g. 'Human'")
 
     scientific_name = models.CharField(
-        max_length=60, null=False, unique=True,
+        max_length=60, unique=True,
         help_text="Organism scientific/binomial name, e.g. 'Homo sapiens'")
 
     # A 'slug' is a label for an object in django, which only contains letters,
@@ -34,9 +34,12 @@ class Organism(models.Model):
     # For more information, see:
     # http://stackoverflow.com/questions/427102/what-is-a-slug-in-django
     slug = models.SlugField(
-        unique=True, null=False,
+        unique=True,
         help_text="URL slug created by calling slugify() on scientific_name in"
         " the management command when the organism is added")
+
+    # Optional field: URL template for genes of this organism
+    url_template = models.URLField(null=True)
 
     # To support Python 2, use "python_2_unicode_compatible" decorator. See:
     # https://docs.djangoproject.com/en/1.11/ref/models/instances/#django.db.models.Model.__str__
