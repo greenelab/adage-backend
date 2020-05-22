5bd1fe94573e58d73f3286805848ae07070f43a1
dongbohu <dongbo.hu@gmail.com>
2020-05-22 06:48:21 -0400

    Tweak migrations

--- adage/genes/migrations/0002_pg_trgm.py
+++ adage/genes/migrations/0002_pg_trgm.py
@@ -1,5 +1,12 @@
+"""
+Enable `pg_trgm` extension in Postgres.  This migration file is equivalent
+to running the following command in psql:
+    CREATE EXTENSION IF NOT EXISTS pg_trgm;
+"""
+
 from django.db import migrations
 from django.contrib.postgres.operations import TrigramExtension
+
 
 class Migration(migrations.Migration):
     dependencies = [('genes', '0001_initial')]
