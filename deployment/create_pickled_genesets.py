7bf93c618c26db0dca4c180957a1af89f16103a4
dongbohu <dongbo.hu@gmail.com>
2020-12-17 21:00:16 -0500

    Add notes

--- deployment/create_pickled_genesets.py
+++ deployment/create_pickled_genesets.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python3
+
 # This Python script reads in `../data/Pseudomonas_aeruginosa_genesets.json`,
 # then pickles the result into `../data/Pseudomonas_aeruginosa_pickled_genesets`,
 # which will be used by the following endpoint:
 # `/api/v1/tribe_client/return_unpickled_genesets?organism=Pseudomonas+aeruginosa`
+
+# Note that if Adage has been configured correctly and Tribe is live, the pickled
+# file can also be created by the following management command in `tribe_client`:
+#   manage.py pickle_public_genesets
+#
+# The current script and `../data/Pseudomonas_aeruginosa_genesets.json` are added
+# to the repo to minimize Adage's dependency on Tribe.
+
 
 import json
 import pickle
